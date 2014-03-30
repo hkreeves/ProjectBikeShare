bf38089f32f434c36d550ba5fbe59a8aa08e9f26
hkreeves <kelvin.h1986@gmail.com>
2014-03-29 20:39:59 -0400

    Added exploration

---
+++ script/dataProcess.R
@@ -0,0 +1,41 @@
+##
+## Project BikeShare
+##
+
+library(plyr)
+
+## EXPLORATION ON DATA
+## a subset of available data corresponding to Q1 2012
+url<- "http://capitalbikeshare.com/assets/files/trip-history-data/2012-1st-quarter.csv"
+download.file(url, "./data/y2012q1.csv")
+
+bs <- read.csv("./data/y2012q1.csv", stringsAsFactors=F)
+str(bs)
+selected.vars <- names(bs)
+selected.vars <- selected.vars[-c(4,7)] # remove the station address text variables
+bs <- bs[selected.vars]
+
+# Processing variables: 1. make Type factors. 
+# 2. convert Start.date and End.date to POSIXt timestamps
+# (joke, already existed) 2. convert Duration to time length
+bs$Type <- as.factor(bs$Type)
+#duration <- ldply(bs$Duration, function(x) as.numeric(strsplit(x, split="[h|m|sec\\.] *")[[1]][1:3]))
+#bs$Duration <- duration[,1] * 360 + duration[,2]* 60 +duration[,3]
+bs$Start.date <- strptime(bs$Start.date, "%m/%d/%Y %H:%M")
+bs$End.date <- strptime(bs$End.date, "%m/%d/%Y %H:%M")
+
+# some plots
+hist(as.Date(bs$Start.date), "days", freq=T)
+freqStart <- sort(table(bs$Start.Term), dec=T)
+freqEnd <- sort(table(bs$End.Term), dec=T)
+
+# popular stations
+plot(stationBook$long, stationBook$lat, type="p", pch=19, col="steelblue2")
+points(stationBook$long[stationBook$term %in% names(freqStart)[1:25]],
+stationBook$lat[stationBook$term %in% names(freqStart)[1:25]],
+col="red", pch=19)
+points(stationBook$long[stationBook$term %in% names(freqEnd)[1:25]],
+stationBook$lat[stationBook$term %in% names(freqEnd)[1:25]],
+col="green", pch=1, cex=1.05)
+
+save(stationBook, bs, freqStart, freqEnd, file="./temp.Rda")
--- script/parseXMLStation.R
+++ script/parseXMLStation.R
@@ -9,8 +9,11 @@ url <- "http://capitalbikeshare.com/data/stations/bikeStations.xml"
 
 bikeStation <- xmlToList(url)
 str(bikeStation)
-# bikeStation has an extra element at the end (.attr). Removed.
-# the 10th element is removalDate which is NULL for a lot the entries. Removed
+# bikeStation has an extra row at the end (.attr). Removed.
+# the 10th variable is removalDate which is NULL for a lot the entries. Removed.
 stationBook <- ldply(bikeStation[-length(bikeStation)], function(x) data.frame(x[-10], stringsAsFactors=F))
 str(stationBook) # 315 obs.
-write.csv(stationBook, "F:/Google Drive/Data Projects/Project BikeShare/stationBook.csv")
+write.csv(stationBook, "F:/Google Drive/Data Projects/ProjectBikeShare/stationBook.csv")
+
+data.date <- date()
+save(stationBook, data.date, file="F:/Google Drive/Data Projects/ProjectBikeShare/stationInfo.Rda")
