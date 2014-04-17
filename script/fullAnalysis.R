3985b464194b0654e34c4f371de3f571118a2d0c
hkreeves <kelvin.h1986@gmail.com>
2014-04-17 15:41:55 -0400

    Major improvement over some functions
    Used {igraph} and {data.table} to speed up the createDistanceMatrix
    function tremedously.
    
    Added new analysis and graphing functions.
    
    Changed fullAnalysis.R accordingly.

--- script/fullAnalysis.R
+++ script/fullAnalysis.R
@@ -23,10 +23,17 @@ bs2012$End.Date <- strptime(bs2012$End.Date, "%m/%d/%Y %H:%M")
 #bs2012$Type <- as.factor(bs2012$Type)
 
 summary(as.Date(bs2012$Start.Date))
+# subset data according to date range or any other criteria
+startDate <- as.Date("2012-03-01")
+endDate <- as.Date("2012-06-30")
+nDay <- as.integer(endDate - startDate + 1)
+bs0 <- subset(bs2012, (as.Date(Start.Date) >= startDate) & (as.Date(End.Date) <= endDate))
+
+# create graph data from the dataset
+bs.graph <- createGraph(bs0, nDay)
 
 # try the createDistanceMatrix function
-# arguments of createDistanceMatrix: data, start.date, end.date, (opt)nday
-res <- createDistanceMatrix(bs2012, "2012-03-01", "2012-06-30")
+res <- createDistanceMatrix(bs.graph, nDay=nDay)
 dm <- res$distMatrix
 util <- res$popStation
 
@@ -35,9 +42,10 @@ util <- res$popStation
 clust <- createCluster(dm, 37, 3)$data
 
 # plot onto a map
-dc.map <- get_map(location="Washinton, DC", zoom=12, maptype="roadmap", 
-color="bw", source="google")
+dc.map <- get_map(location="Washinton, DC", zoom=13, maptype="roadmap", 
+color="color", source="google")
 
 bs.plot <- plotCluster(clust, util)
+bs.plot <- plotLink(bs.graph, clust, 20, bs.plot) 
 print(bs.plot)
 
--- script/functions.R
+++ script/functions.R
@@ -6,83 +6,64 @@
 ## April 13th, 2014
 ##
 
+library(data.table)
+library(plyr)
 library(fpc)
 library(RColorBrewer)
 library(ggmap)
 
-# create distanc matrix for data, optionally within a date range
-createDistanceMatrix <- function(bs, beginDate=min(as.Date(bs$Start.Date)), 
-endDate=max(as.Date(bs$End.Date)), nDay=NA){
-  # if nDay is not specified, meaning that the date range is continuous
-  if(is.na(nDay)){
-    beginDate <- as.Date(beginDate)
-    endDate <- as.Date(endDate)
-    nDay <- as.numeric(endDate - beginDate + 1)
-    bs0 <- subset(bs, (as.Date(Start.Date) >= beginDate) & (as.Date(Start.Date) <= endDate))}
-    
+# create graph data in the following format: terminal1, terminal2, frequency
+createGraph <- function(bs0, nDay){
   nWeek <- nDay/7
-  #print(str(bs0))
-
-  # create path IDs formated as "StartTerminal-EndTerminal" and the opposite
-  bs0$S2E <- paste(bs0$Start.Term, bs0$End.Term, sep="-")
-  bs0$E2S <- paste(bs0$End.Term, bs0$Start.Term, sep="-")
 
   # find the frequency of each path ID
-  link1.count <- table(bs0$S2E)
-  link1 <- as.data.frame.table(link1.count)
-  link2.count <- table(bs0$E2S)
-  link2 <- as.data.frame.table(link2.count)
+  # we use data.table to preform aggregation and sorting
+  bs0 <- as.data.table(bs0)
+  bs0 <- bs0[, .N, by=c("Start.Terminal", "End.Terminal")]
+  bs0 <- bs0[order(Start.Terminal, End.Terminal)]
 
-  # merge frequencies between "A-B" and "B-A"
-  link.count <- merge(link1, link2, by="Var1", all=T)
-  link.count[is.na(link.count)] <- 0
+  bs0$N <- bs0$N/nWeek
+  bs0 <- as.data.frame(bs0)
+  names(bs0) <- c("Term1", "Term2", "Freq")
+ 
+  # return 
+  return(bs0)
+}
 
-  link.count$total <- link.count$Freq.x + link.count$Freq.y
+# create distanc matrix for data
+createDistanceMatrix <- function(bs.graph, nDay){    
+  nWeek <- nDay/7
 
-  # full list of all possible path IDs
-  all.term <- sort(unique(c(bs0$Start.Term, bs0$End.Term)))
-  all.pair <- cbind(rep(all.term, each=length(all.term)), rep(all.term, length(all.term)))
-  full.link <- data.frame(list(Var1=paste(all.pair[,1], all.pair[,2], sep="-"), Freq=rep(0, nrow(all.pair))))
-
-  # merge link.count to full.link
-  full.link <- merge(full.link, link.count, by="Var1", all.x=T)
-  full.link[is.na(full.link)] <- 0
-  full.link$Freq <- full.link$Freq.x + full.link$Freq.y
-  full.link <- full.link[,1:2]
-  nz.full.link <- subset(full.link, Freq > 0)
-  message("Statistics of #exchanges per 7 days for each path:") 
-  print(quantile(nz.full.link$Freq/nWeek, seq(0.9, 1, 0.02)))
-  message(paste("mean #exchanges per 7 days:", round(mean(full.link$Freq/nWeek), 4)))
-
-  # split the pathID back to Start, End
-  # compute the "distance" using the following formula: dist = 1/(Freq/nWeek + 1)
-  triples <- data.frame(cbind(all.pair, 1/(full.link$Freq/nWeek + 1)))
-  names(triples) <- c("Term1", "Term2", "Dist")
-
-  # reshape the long data to wide
-  dist.mtrx <- reshape(triples, timevar="Term1", idvar="Term2", direction="wide")
+  ######
+  # construct distance matrix (adjacent matrix) using igraph
+  ######
+  
+  bs.g <- graph.data.frame(bs.graph)
+  
+  # use get.adjacency() to obtain the adjacent matrix  
+  dist.mtrx <- as.matrix(get.adjacency(bs.g, attr="Freq")) 
+  dist.mtrx <- dist.mtrx + t(dist.mtrx)
+  
+  # convert frequency to distance using dist=1/(Freq/nWeek + 1)
+  dist.mtrx <- 1/(dist.mtrx + 1)
+  dist.mtrx <- data.frame(as.integer(rownames(dist.mtrx)), dist.mtrx)
+  names(dist.mtrx)[1] <- "TermID"
   
   # get utilization of each station
-  pop.base <- data.frame(list(Var1=all.term, Freq=0))
-  pop.start <- as.data.frame.table(table(bs0$Start.Term)/nDay)
-  pop.start <- merge(pop.base, pop.start, by="Var1", all.x=T)
-  pop.start[is.na(pop.start)] <- 0
-  pop.start$Freq <- pop.start$Freq.x + pop.start$Freq.y
-  pop.start <- pop.start[, c("Var1", "Freq")]
+  all.term <- sort(as.integer(V(bs.g)$name))
+  pop.station <- data.frame(list(TermID=all.term, Freq=0))
+  pop.start <- tapply(bs.graph$Freq, bs.graph$Term1, sum) / 7
+  pop.start <- data.frame(list(TermID=names(pop.start), Out=pop.start))
+  pop.station <- merge(pop.station, pop.start, by="TermID", all.x=T)
 
-  pop.end <- as.data.frame.table(table(bs0$End.Term)/nDay)
-  pop.end <- merge(pop.base, pop.end, by="Var1", all.x=T)
-  pop.end[is.na(pop.end)] <- 0
-  pop.end$Freq <- pop.end$Freq.x + pop.end$Freq.y
-  pop.end <- pop.end[, c("Var1", "Freq")]
-
-  pop.station <- pop.start
-  pop.station$Freq <- pop.start$Freq + pop.end$Freq
-
-  colnames(pop.station) <- c("TermID", "Freq")
+  pop.end <- tapply(bs.graph$Freq, bs.graph$Term2, sum) / 7
+  pop.end <- data.frame(list(TermID=names(pop.end), In=pop.end))
+  pop.station <- merge(pop.station, pop.end, by="TermID", all.x=T)
+  pop.station[is.na(pop.station)] <- 0
+  pop.station$Freq <- pop.station$In + pop.station$Out 
 
   # return distance matrix and utilization
-  return(list(distMatrix=dist.mtrx[], popStation=pop.station))
+  return(list(distMatrix=dist.mtrx, popStation=pop.station))
 }
 
 # perform clustering analysis using DBSCAN algorithm
@@ -92,12 +73,12 @@ createCluster <- function(dist, threshold, minNeighbor){
   message("Cluster Assignment:")
   print(table(cluster1$cluster))
   
-  bsClust <- data.frame(list(TermID=dist$Term2, Cluster=cluster1$cluster))
+  bsClust <- data.frame(list(TermID=dist$TermID, Cluster=cluster1$cluster))
   return(list(data=bsClust, clusterObject=cluster1))
 }
 
 # plot on a map
-plotCluster <- function(clust, util, base=dc.map){
+plotCluster <- function(clust, util, base=dc.map, noise=T){
   # combine clust and util
   # then merge clust and stationBook on the common variable "TermID"
   clust <- cbind(clust, util$Freq)
@@ -112,7 +93,7 @@ plotCluster <- function(clust, util, base=dc.map){
   # clusters
   clust.names <- names(table(clust$Cluster))
   clust.names <- paste("Comm", clust.names)
-  clust.names[1] <-  "Unclustered"
+  if(noise)  clust.names[1] <-  "Unclustered"
 
   # color scheme
   color <- c("grey32", brewer.pal(9, "Set1"))
@@ -126,10 +107,51 @@ plotCluster <- function(clust, util, base=dc.map){
   station.map <- 
     ggmap(dc.map, extend="device") +
     geom_point(
-      aes(x=long, y=lat, col=factor(Cluster), size=sqrt(Freq + 1)),
-      data=station, alpha=0.5) +
-    scale_colour_manual(values=color, name="Community", labels=clust.names) +
+      aes(x=long, 
+        y=lat, 
+        fill=factor(Cluster), 
+        size=sqrt(Freq)),
+      shape=21,
+      color="darkblue",
+      data=station, 
+      alpha=0.9) +
+    scale_fill_manual(values=color, name="Community", labels=clust.names) +
     scale_size_area(breaks=sqrt(area.scale), labels=area.scale, name="Exchanges per day") +
     theme(axis.title=element_blank())
+}
+
+# draw lines for frequent paths on top of the map plot
+# "frequent" is specified by a minimum cutoff, minEx
+plotLink <- function(bs.graph, clust, minEx=40, base.plot){
+  # sum up A-B and B-A
+  bs.graph <- merge(bs.graph, bs.graph, by.x=c("Term1", "Term2"), by.y=c("Term2", "Term1"))
+  bs.graph$Freq <- bs.graph$Freq.x + bs.graph$Freq.y
+  bs.graph <- bs.graph[-c(3,4)]
+
+  station <- merge(bs.graph, stationBook[c("terminalName", "lat", "long")], 
+    by.x="Term1", by.y="terminalName", all=F)
+  station <- merge(station, stationBook[c("terminalName", "lat", "long")], 
+    by.x="Term2", by.y="terminalName", suffixes = c("", ".b"), all=F)
+  station <- merge(station, clust, by.x="Term1", by.y="TermID")
+
+  # subset bs.graph
+  station <- subset(station, (Term2 > Term1) & (Freq >= minEx))
+
+  bs.plot2 <-  base.plot +
+    geom_segment(
+      aes(x = long, 
+        y = lat, 
+        xend = long.b, 
+        yend = lat.b, 
+        color=factor(Cluster), 
+        size=Freq/100), 
+      alpha=0.4, 
+      lineend="butt", 
+      data=station,
+      show_guide=F,
+    ) + 
+    scale_color_manual(values=color) 
+
+  return(bs.plot2)
 }
 
